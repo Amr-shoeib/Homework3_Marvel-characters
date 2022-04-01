aa7b189938cb3c11935f829ac7f414e2f5c8ca37
Amr-shoeib <amr79.a@gmail.com>
2022-04-01 02:54:43 -0400

    Answer the 5 questions about marvel character

--- homework_3/components/quizQuestions.py
+++ homework_3/components/quizQuestions.py
@@ -9,5 +9,31 @@ questions = {
         "question" : "Are you human? ",
         "yes" : 0,
         "no" : 10
-    }
+    },
+    
+     "q3" : {
+        "question" : "Are you going to save the world? ",
+        "yes" : 0,
+        "no" : 10
+    },
+     
+      "q4" : {
+        "question" : "have you ever thought of getting rid of your superpowers? ",
+        "yes" : 0,
+        "no" : 10
+    },
+      
+       "q4" : {
+        "question" : "will you live forever? ",
+        "yes" : 0,
+        "no" : 10
+    },
+       
+        "q5" : {
+        "question" : "will you marry me? ",
+        "yes" : 0,
+        "no" : 10
+    },
+
+       
 }
--- homework_3/components/vars.py
+++ homework_3/components/vars.py
@@ -5,4 +5,5 @@ characters = ["Hulk", "Thor", "Black Widow", "Captain America"]
 
 #optional variable for if you decide to use the Pillow package to display images
 #you will need to add these images to this project
-images = ["hulk.jpg", "thor.jpg", "natasha.jpg", "cap.jpg"]
+images = ["hulk.jpg", "thor.jpg", "natasha.jpg", "Captin America.jpg"]
+
