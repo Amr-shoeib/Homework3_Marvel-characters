226b8d04e0e179e9659209a5ee6ebe01db63a07d
Amr-shoeib <amr79.a@gmail.com>
2022-04-01 11:39:31 -0400

    Update quizTally.py

--- homework_3/components/quizTally.py
+++ homework_3/components/quizTally.py
@@ -9,6 +9,9 @@ def total(value):
 
     if value <= 10:
         vars.character = vars.characters[0]
+        
+    elif value >= 10:
+        vars.character = vars.characters[0]
 
         print("It's :fire:, :punch:,:muscle:"+ vars.character)
         # add some emoji icons, or show the character image using the Pillow package
